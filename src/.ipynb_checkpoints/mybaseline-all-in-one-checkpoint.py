48ca0519b73180f6881a18893b89e8e208c6505d
Wang Yuan <32762885+ywang037@users.noreply.github.com>
2021-04-28 12:56:37 +0800

    Update mybaseline-all-in-one-checkpoint.py

--- src/.ipynb_checkpoints/mybaseline-all-in-one-checkpoint.py
+++ src/.ipynb_checkpoints/mybaseline-all-in-one-checkpoint.py
@@ -11,17 +11,19 @@ from torchvision import datasets
 # from data_preparation import data_setup
 
 import time
+import csv
+import numpy as np
 
 class TaskMnist():
     def __init__(self, nn='cnn_wy'):
-        self.path = '..\data\mnist'
+        self.path = './data/mnist'
         self.name = 'mnist'
         self.nn = nn
         
 class TaskCifar():
-    def __init__(self,nn='torch'):
-        self.path = '..\data\cifar'
-        self.name = 'cifar10'
+    def __init__(self,nn='cnn_torch'):
+        self.path = './data/cifar'
+        self.name = 'cifar'
         self.nn = nn
 
 class HyperParam():
@@ -182,6 +184,11 @@ def data_cifar(path, batch_size=64):
         transforms.ToTensor(),
         transforms.Normalize(mean_0,std_0),
     ])
+    # configure transform for test data
+    transform_test_0 = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean_0,std_0),
+    ])
 
     # enhanced transform, random crop and flip is optional
     transform_train_1 = transforms.Compose([
@@ -199,7 +206,7 @@ def data_cifar(path, batch_size=64):
     ])
 
     # configure transform for test data
-    transform_test = transforms.Compose([
+    transform_test_1 = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean_1,std_1),
     ])
@@ -209,7 +216,7 @@ def data_cifar(path, batch_size=64):
     loader_train = data.DataLoader(data_train, batch_size=batch_size, shuffle=True)
 
     # setup the CIFAR10 test dataset
-    data_test = datasets.CIFAR10(root=path, train=False, download=False, transform=transform_test)
+    data_test = datasets.CIFAR10(root=path, train=False, download=False, transform=transform_test_0)
     loader_test = data.DataLoader(data_test, batch_size=100, shuffle=False)
 
     return loader_train, loader_test
@@ -232,13 +239,56 @@ def data_mnist(path,batch_size=64):
 # the function used to count the number of trainable parameters
 def get_count_params(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
-        
+
+# training function
+def train_model(loader_train, loader_test, epochs, loss_fn, optimizer, device):
+    with open('./save/results-mybaseline.csv', 'w', newline='') as file:
+        writer = csv.writer(file,delimiter=',')
+        writer.writerow(['epoch','training loss', 'test acc'])
+        for epoch in range(1, epochs+1):
+            train_loss = 0.0
+            test_loss = 0.0
+            test_acc = 0.0
+
+            # training of each epoch
+            model.train()
+            for batch, (images, labels) in enumerate(loader_train):
+                images, labels = images.to(device), labels.to(device)
+                optimizer.zero_grad()
+                outputs = model(images)
+                loss = loss_fn(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                train_loss += loss.item() * images.size(0)
+            train_loss /= len(loader_train.dataset)
+
+            # test after each epoch
+            model.eval()
+            num_correct = 0 
+            with torch.no_grad():
+                for batch, (images, labels) in enumerate(loader_test):
+                    images, labels = images.to(device), labels.to(device)
+                    outputs = model(images)
+                    loss = loss_fn(outputs,labels)
+                    test_loss += loss.item() * images.size(0)
+                    pred = outputs.argmax(dim=1)
+                    num_correct += pred.eq(labels.view_as(pred)).sum().item()
+            test_loss /= len(loader_test.dataset)
+            test_acc = 100*num_correct/len(loader_test.dataset)
+            print('Epoch: {:>4}/{} | Training Loss: {:.2f} | Test Loss: {:.2f} | Test accuracy = {:.2f}%'.format(epoch, epochs, train_loss, test_loss, test_acc))
+            writer.writerow([
+                epoch, 
+                np.round(train_loss,2), 
+                np.round(test_acc,2)
+                ])
+
 if __name__ == '__main__':
+    torch.manual_seed(1)
     # configure the task and training settings
-    task = TaskMnist(nn='cnn')    
-    settings = HyperParam(path=task.path)
+    # task = TaskMnist(nn='2nn_wy')    
+    task = TaskCifar(nn='cnn_torch')
+    settings = HyperParam(path=task.path, learning_rate=0.1, epoch=100, nesterov=False)  
     
-    start = time.time()
     if task.name == 'mnist':
         if task.nn == 'cnn_wy':
             model = CNNMnistWy().to(settings.device)
@@ -250,7 +300,7 @@ if __name__ == '__main__':
             model = MLP().to(settings.device)
         loader_train, loader_test = data_mnist(path=settings.datapath,batch_size=settings.bs)
     elif task.name == 'cifar':
-        if task.cnn == 'torch':
+        if task.nn == 'cnn_torch':
             model = CNNCifarTorch().to(settings.device)
         else:
             model = CNNCifarTf().to(settings.device)
@@ -267,8 +317,6 @@ if __name__ == '__main__':
     print('\nModel training initiated...\n')
     print(f'Dataset:\t{task.name}')
     print(f'Loss function:\t{loss_fn}')
-#     optimizer_selection = 'SGD with Nesterov momentum=0.9' if settings.nesterov else 'vanilla SGD'
-#     print('Optimizer:\t',optimizer_selection)
     print('Optimizer:\tSGD with Nesterov momentum=0.9') if settings.nesterov else print('Optimizer:\tvanilla SGD')
     print(f'learning rate:\t{settings.lr}')
     print(f'Batch size:\t{settings.bs}')
@@ -277,38 +325,20 @@ if __name__ == '__main__':
     print(f'Trainable model parameters:\t{get_count_params(model)}')
 
     # start training
-    for epoch in range(1, settings.epoch+1):
-        train_loss = 0.0
-        test_loss = 0.0
-        test_acc = 0.0
-        
-        # training of each epoch
-        model.train()
-        for batch, (images, labels) in enumerate(loader_train):
-            images, labels = images.to(settings.device), labels.to(settings.device)
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = loss_fn(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item() * images.size(0)
-        train_loss /= len(loader_train.dataset)
-
-        # test after each epoch
-        model.eval()
-        num_correct = 0 
-        with torch.no_grad():
-            for batch, (images, labels) in enumerate(loader_test):
-                images, labels = images.to(settings.device), labels.to(settings.device)
-                outputs = model(images)
-                loss = loss_fn(outputs,labels)
-                test_loss += loss.item() * images.size(0)
-                pred = outputs.argmax(dim=1)
-                num_correct += pred.eq(labels.view_as(pred)).sum().item()
-        test_loss /= len(loader_test.dataset)
-        test_acc = 100*num_correct/len(loader_test.dataset)
-        print('Epoch: {} | Training Loss: {:.2f} | Test Loss: {:.2f} | Test accuracy = {:.2f}%'.format(epoch, train_loss, test_loss, test_acc))
+    start = time.time()
+    train_model(loader_train=loader_train,
+                loader_test=loader_test,
+                loss_fn=loss_fn,
+                optimizer=optimizer,
+                epochs=settings.epoch,
+                device=settings.device)
 
     # print the wall-clock-time used
     end=time.time() 
     print('\nWall clock time elapsed: {:.2f}s'.format(end-start))
+    # if settings.nesterov:
+    #     save_path = f'./save/weights-{task.nn}-{task.name}-ep{settings.epoch}-bs{settings.bs}-lr{settings.lr}-nag.pth'
+    # else:
+    #     save_path = f'./save/weights-{task.nn}-{task.name}-ep{settings.epoch}-bs{settings.bs}-lr{settings.lr}.pth'
+    save_path = f'./save/weights-mybaseline-{task.nn}-{task.name}-ep{settings.epoch}-bs{settings.bs}-lr{settings.lr}.pth'
+    torch.save(model.state_dict(), save_path)
