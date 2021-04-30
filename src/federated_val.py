fcb8707bcab3ceeaf52e96264920589a575e1a76
Wang Yuan <32762885+ywang037@users.noreply.github.com>
2021-04-30 11:08:17 +0800

    Updates training scripts

--- src/federated_val.py
+++ src/federated_val.py
@@ -70,6 +70,12 @@ if __name__ == '__main__':
     global_model.train()
     print(global_model,'\n')
 
+    # pause and print message for user to confirm the hyparameter are good to go
+    answer = input("Press n to abort, press any other key to continue, then press ENTER: ")
+    if answer == 'n':
+        exit('\nTraining is aborted by user')
+    print('\nTraining starts...\n')
+
     # copy weights
     global_weights = global_model.state_dict()
 
