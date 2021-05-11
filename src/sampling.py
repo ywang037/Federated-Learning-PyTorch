4f652c3cd8bffde18cf7c6aca608065cdc1df4b8
Wang Yuan <32762885+ywang037@users.noreply.github.com>
2021-05-11 16:14:48 +0800

    Update sampling.py

--- src/sampling.py
+++ src/sampling.py
@@ -17,8 +17,10 @@ def mnist_iid(dataset, num_users):
     num_items = int(len(dataset)/num_users)
     dict_users, all_idxs = {}, [i for i in range(len(dataset))]
     for i in range(num_users):
+        # randomly drawn image indices for i-th client from all image indices, with probability of uniform distriubtion
         dict_users[i] = set(np.random.choice(all_idxs, num_items,
                                              replace=False))
+        # exclude the image indices that have been drawn
         all_idxs = list(set(all_idxs) - dict_users[i])
     return dict_users
 
@@ -115,7 +117,6 @@ def mnist_noniid_unequal(dataset, num_users):
                     (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]),
                     axis=0)
     else:
-
         for i in range(num_users):
             shard_size = random_shard_size[i]
             rand_set = set(np.random.choice(idx_shard, shard_size,
