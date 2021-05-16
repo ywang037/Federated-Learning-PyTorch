3557d260ac8be445b4f0581585cb456cd325a92d
Wang Yuan <32762885+ywang037@users.noreply.github.com>
2021-05-16 10:19:55 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -40,6 +40,7 @@ def get_dataset(args):
         train_transform = transforms.Compose([
             transforms.RandomCrop(32,padding=4),
             transforms.RandomHorizontalFlip(),
+            # transforms.ColorJitter(brightness=0.2,contrast=0.2,saturation=0.2),
             transforms.ToTensor(),
             transforms.Normalize(mean, std)
             ])
